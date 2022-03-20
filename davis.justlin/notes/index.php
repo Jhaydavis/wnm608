cc1cd0996bcaec674c37dc34bf6d2285a59a7d97
Jhay Davis <Jhaydavis@mac.com>
2022-03-20 13:51:10 -0500

    Updated to pull sync with local php dev environment in MAMP
    Updated style guide and updated repo to link to local dev environment in MAMP

---
+++ davis.justlin/notes/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo '<h1>Welcome Jhay!</h1>' ?>
+</body>
+
+</html>
